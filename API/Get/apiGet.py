1dcbc94680e4f2ed1f80574111d5c690ca9118bf
fernado1981 <66782895+fernado1981@users.noreply.github.com>
2020-07-22 11:13:06 +0200

    Update apiGet.py

--- API/Get/apiGet.py
+++ API/Get/apiGet.py
@@ -8,14 +8,8 @@ class get:
     ExpectedJson = {'userId': 1, 'id': 1, 'title': 'delectus aut autem', 'completed': False}
     # get del listado filtrado por UserId:1
     url = 'https://jsonplaceholder.typicode.com/todos/1'
-    #hacemos la peticion get pasandole la url
-    response = requests.get(url)
-    #imprimimos el status
-    print(response.status_code)
-    #parseamos a json la response
-    responseJson = response.json()
     #creamos un objeto de tipo Get y la pasamor el responseJson y el ExpectedJson al constructor
-    Param = Get(responseJson, ExpectedJson)
+    Param = Get(url, ExpectedJson)
     #invocamos al método comparation de Get
     Param.comparation()
     #Añadimos valores al dic obtenido
