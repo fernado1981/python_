d0eb28cd09b43f3d7d6b738157f5ee0997cb2139
fernado1981 <66782895+fernado1981@users.noreply.github.com>
2020-07-22 10:58:22 +0200

    Update PostData.py

--- API/PostPrueba/PostData.py
+++ API/PostPrueba/PostData.py
@@ -8,17 +8,25 @@ class postData:
     data = {}
     lista = []
 
+    #generamos el constructor para inicializar las variables necesarias
     def __init__(self, url, data):
         self.url = url
         self.data = data
-
+    
+    #creamos el método para añadir el data por medio de Post
     def AddData(self):
         print(self.data)
+        #generamos la petición post pasandole la url, el data y strema=True
         response = requests.post(self.url, self.data, stream=True)
+        #Imprimimos el status
         print(response.status_code)
+        #parseamos la respuesta a json
         response = response.json()
+        #Añadimos la response a la lista
         self.lista.append(response)
 
+        #recorremos la lista por posición
         for i in range(len(self.lista)):
             print(i)
+            #imprimimos la lista para comprobar que se ha realizado correctamente
             print(self.lista[i]['Nombre'], '', self.lista[i]['Edad'])
