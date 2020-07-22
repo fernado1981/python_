a591cafcef3e19823f07d118493561cbe2e887d9
fernado1981 <66782895+fernado1981@users.noreply.github.com>
2020-07-22 10:53:36 +0200

    Update Searchdata.py

--- API/GetPrueba/Searchdata.py
+++ API/GetPrueba/Searchdata.py
@@ -9,24 +9,36 @@ class Searchdata:
     response = ''
     data = ''
     lista = []
-
+    
+    #inicializamos la variables necesarias con los datos pasados al contructor
     def __init__(self, url, data):
         self.url = url
         self.data = data
 
+     #Método de busqueda
     def searchApiGet(self):
+        #petició
+    def searchApiGet(self):
+        #peticion get a la url dada
         self.response = requests.get(self.url)
+        #se imprime el status
         print(self.response.status_code)
-
+        
+        #se transforman los datosa obtenidos a json 
         self.responseJson = self.response.json()
+        #se añaden a la lista
         self.lista.append(self.responseJson)
 
+        #recorremos el array por posición
         for i in range(len(self.lista)):
-            print(self.lista[i])
+            #recorremos las listas del array por clave valor
             for c, v in self.lista[i].items():
-                print(c, v)
+                #buscamos si coincide la clave con el elemento a buscar
                 if c == self.data:
+                    #si coincide lo metemos en la lista en la posicion de i
                     self.lista[i] = c
+                    #comprobamos que no este vacia la lista
                     if len(self.lista) >= 1:
+                        #imprimimos la lista
                         print(self.lista)
         print(self.lista)
