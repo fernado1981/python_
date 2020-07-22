6825d952ccf3050cfcbf6e6a2510ff6d10bfda90
fernado1981 <66782895+fernado1981@users.noreply.github.com>
2020-07-22 11:17:05 +0200

    Update Get.py

--- API/Get/Get.py
+++ API/Get/Get.py
@@ -2,17 +2,27 @@ class Get:
     dicRes = {}
     dicExpect = {}
     value = ''
-
+    url=''
+    response=''
+    
     #constructor para inicializar las variables necesarias
-    def __init__(self, responseDic, expectedResponse):
-        self.dicRes = responseDic
+    def __init__(self, url, expectedResponse):
+        self.url = url
         self.dicExpect = expectedResponse
 
+    def obtGet(self):
+        #hacemos la peticion get pasandole la url
+        response = requests.get(self.url)
+        #imprimimos el status
+        print(response.status_code)
+        #parseamos a json la response
+        self.responseJson = response.json()
+        
     #método para eleminar clave:valor del diccionario   
     def deleteValue(self, item, val):
-        for c, v in self.dicRes.items():
-            if self.dicRes[item] == val:
-                del self.dicRes[item]
+        for c, v in self.responseJson.items():
+            if self.responseJson[item] == val:
+                del self.responseJson[item]
                 print("Eliminado", item, "con valor :", val)
                 break
             else:
@@ -22,19 +32,19 @@ class Get:
     #Añadimos valores por clave:valor al diccionario    
     def addValue(self, item, val):
         print("pasas item: ", item, " valor: ", val)
-        if item in self.dicRes:
+        if item in self.responseJson:
             print("El ", item, " con el valor ", val, "ya existe en el diccionario")
         else:
             print("Añadimos elemento")
-            self.dicRes[item] = val
+            self.responseJson[item] = val
     
     #comparamos el response obtenido con lo experado
     def comparation(self):
-        if self.dicRes == self.dicExpect:
+        if self.responseJson == self.dicExpect:
             print("El resultado del dicionario con el experado son iguales")
         else:
             print("Diferentes")
     
     #mostramos el resultado
     def showResult(self):
-        print(self.dicRes)
+        print(self.responseJson)
