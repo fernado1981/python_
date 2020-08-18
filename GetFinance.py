63303096e2559aefafcd2a8d57475e8a855b3711
fernado1981 <66782895+fernado1981@users.noreply.github.com>
2020-08-18 14:51:46 +0200

    Update GetFinance.py

--- GetFinance.py
+++ GetFinance.py
@@ -1,6 +1,6 @@
 import requests
 
-response = requests.get('https://finnhub.io/api/v1/quote?symbol=GIB&token=bsrbhmf48v6tucpg28a0')
+response = requests.get('https://finnhub.io/api/v1/quote?symbol=AAPL&token=bsrbhmf48v6tucpg28a0')
 print(response.status_code)
 
 if response.status_code == 200:
