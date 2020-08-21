0684ba1c81373574c181fbc63e537fb590954665
fernixq981 <fernando.manrique.villanueva@gmail.com>
2020-08-21 09:49:29 +0200

    update

--- Selenium/Ejercicios_propuestos/Prueba_QA_18/eject.py
+++ Selenium/Ejercicios_propuestos/Prueba_QA_18/eject.py
@@ -1,5 +1,6 @@
 from Selenium.Ejercicios_propuestos.Prueba_QA_18.Api.apiGet import apiGet
 from Selenium.Ejercicios_propuestos.Prueba_QA_18.NewsPaper.periodicos import periodicos
+from Selenium.Ejercicios_propuestos.Prueba_QA_18.Teleco.telefonica import telefonica
 
 
 class eject:
@@ -8,8 +9,20 @@ class eject:
     #p1.searchdata(value)
     #p1.showData()
 
-   p2 = periodicos()
-   p2.setNews('ElMundo')
-   url = p2.takeUrl()
-   p2.openUrl(url)
-   p2.exit()
+   #p2 = periodicos()
+   #p2.setNews('ElMundo')
+   #url = p2.takeUrl()
+   #p2.openUrl(url)
+   #p2.exit()
+
+    P3 = telefonica()
+    P3.openUrl()
+    P3.inputName()
+    P3.firstOption()
+    P3.cookie()
+    P3.takeTag()
+    P3.acciones()
+    P3.switchFrame()
+    P3.changeValueNY()
+    P3.getValueNY()
+    P3.exit()
