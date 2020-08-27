dfb7f47ff179b8755743807821ef52e5cf1dc5bd
fernixq981 <fernando.manrique.villanueva@gmail.com>
2020-08-27 10:40:32 +0200

    update

---
+++ Selenium/Ejercicios_propuestos/Prueba_QA_20/lanzador.py
@@ -0,0 +1,12 @@
+from Selenium.Ejercicios_propuestos.Prueba_QA_20.teleco import teleco
+
+
+class lanzador():
+    p = teleco()
+    p.opneBrowser()
+    p.firstOption()
+    p.coockies()
+    p.takeTag()
+    p.Accionistas()
+    p.valueNY()
+    p.exit()
