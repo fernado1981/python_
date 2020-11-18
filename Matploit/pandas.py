7a278f9363740c1ea80134f8d38b080221878e98
fernixq981 <fernando.manrique.villanueva@gmail.com>
2020-11-18 19:43:44 +0100

    update

---
+++ Matploit/pandas.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+data = {'india': [8880, 8670, 8147, 7338, 5704],
+        'china': [5123, 6682, 3308, 1863, 1527],
+        'year': [1983, 1984, 1985, 1986, 1987]}
+df = pd.DataFrame()
+print(df)
---
+++ Matploit/point.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+# pintamos un punto en (5x,5y)
+plt.plot(5, 5, 'o')
+# punemos labels para x y para y
+plt.ylabel('Y')
+plt.xlabel('X')
+# damos un nombre al gráfico:
+plt.title('punto 5,5')
+plt.show()
